27e600dfeeceba53677f3fb06bccd15d58a9d544
Aleah Querns <59670334+akquerns@users.noreply.github.com>
2022-02-24 10:46:11 -0600

    class 2-24

--- CODE/22-2-22_class9.R
+++ CODE/22-2-22_class9.R
@@ -58,9 +58,9 @@ cnty_pts <- st_centroid(bea_test) #will give warning--maybe want to switch to eq
 
 st_crs(bea_test)
 
-bea_test%>%
+bea_sf%>%
   ggplot(.)+
-  geom_sf(aes(fill=perc_total), alpha=0.7) +
+  geom_sf(aes(fill=total), alpha=0.7) +
   theme_minimal() +
   geom_sf(data=cnty_pts, aes(size=Ag))+
   facet_wrap(~Year)+
@@ -69,3 +69,51 @@ bea_test%>%
         axis.ticks=element_blank(),
         panel.grid.major = element_blank()) +
   scale_fill_viridis_c()
+
+bea_sf %>%
+  filter(Year==2012)%>%
+  ggplot(.)+
+  geom_sf(aes(fill=Mining))+
+  geom_sf_label(aes(label = name), label.padding = unit(0.2, "lines"), size=3)+
+  scale_fill_viridis_c(direction=-1)
+
+bea_sf %>%
+  filter(Year==2012)%>%
+  st_transform(., "+proj=lcc +lat_1=38.71666666666667 +lat_2=39.78333333333333 +lat_0=38.33333333333334 +lon_0=-98 +x_0=399999.9999999999 +y_0=0 +ellps=GRS80 +datum=NAD83 +to_meter=0.3048006096012192 +no_defs") %>%
+  ggplot(.)+
+  geom_sf(aes(fill=Mining))+
+  geom_sf_label(aes(label = name), label.padding = unit(0.2, "lines"), size=3)+
+  scale_fill_viridis_c(direction=-1)+
+  theme_minimal()+
+  theme(axis.text=element_blank(),
+        axis.title = element_blank(),
+        legend.position="bottom")
+
+
+plot(bea_sf["total"]) # base R plotting
+
+plot(bea_sf["total"], pal = c('#edf8b1','#7fcdbb','#2c7fb8'),
+     nbreaks=3,
+     breaks="jenks",
+     main= "TITLE",
+     key.pos=2) # base R plotting, improved
+
+library(tmap)
+
+tmap_mode("plot")
+
+
+qtm(bea_sf, fill = "Mining",
+    legend.outside=TRUE,
+    legend.outside.position= "bottom",
+    fill.title = "Mining Economic Production",
+    style= "natural") # check help for style colors
+
+
+tm_shape(bea_sf %>% filter(Year==2012))+
+  tm_polygons("Mining", title= "Mining $") +
+  tm_bubbles("Ag")+
+  tm_layout(legend.outside=TRUE,
+            inner.margins = c(.2, .05, .05, .1)) +
+  tm_compass(size=0.5, type = "rose",
+             position= c("right", "bottom"))
